b1851c25fbcc547e9610722d743598ef23a923a5
Cat1101 <86596934+Cat1101@users.noreply.github.com>
2022-04-24 00:20:40 +0300

    Update AppSettings.cs

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -6,5 +6,5 @@ public static class AppSettings
 
     public static string Name { get; set; } = "my_asp_kot_bot";
 
-    public static string Key { get; set; } = "5387774647:AAEM0G7y18mLg6vp2mGCQxETuJv80YXqSlU";
+    public static string Key { get; set; } = "";
 }
